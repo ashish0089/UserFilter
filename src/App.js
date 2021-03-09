127468c6e2bc53bf9d1ec523554d453dff73cbdf
ashish21.kumar <ashish21.kumar@lge.com>
2021-03-09 19:14:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect,useState} from 'react'
+
 
 function App() {
+
+const [users,setUsers] = useState();
+const [searchedUser,setSearchedUser] = useState();
+  
+  useEffect(() =>{
+    const users =   fetch('http://localhost:5000/api/users').then((json)=>{
+        return json.json();
+      }).then((result)=>{
+          // console.log(result);
+          setUsers(result)
+      })
+
+     // console.log(users);
+  },[])
+
+  const getUser = (e) =>{
+    let searchItem = e.target.value;
+    let filterUser= [];
+    if(searchItem != ''){
+      filterUser = users.filter((user) =>{
+        return user.name.indexOf(searchItem) !=-1 || user.userId.indexOf(searchItem) != -1
+      })
+    }else{
+      filterUser=[];
+    }
+    
+    console.log(filterUser);
+    setSearchedUser(filterUser);
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+          <input type ="text" placeholder="search" onChange={(e) =>getUser(e)}></input>
+          <button>Cancel</button>
+      
+          {
+            searchedUser && searchedUser.map((user)=>{
+              return <div><b>{user.name}</b>, 
+              <span className="display">{user.userId},</span> 
+              <span className="display">{user.desg}</span>
+              </div>
+            })
+          }
     </div>
   );
 }
 
-export default App;
+export default App;   
